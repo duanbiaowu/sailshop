103572fa268a7da2c858340a01e682977c01319f
duanbiaowu <codean.net@gmail.com>
2016-07-27 18:26:52 +0800

    后台引入Vue.js

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
         'css/font-awesome.min.css'
     ];
     public $js = [
+        'js/vue.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
